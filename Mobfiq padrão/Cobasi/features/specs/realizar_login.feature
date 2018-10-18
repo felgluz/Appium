71b47f98bd96a1d74ddb6f6dabe92d78d77bea52
Felipe Luz <felgluz@gmail.com>
2018-10-18 19:06:42 -0300

    Primeiro caso de teste automatizado

--- Mobfiq padrão/Cobasi/features/specs/realizar_login.feature
+++ Mobfiq padrão/Cobasi/features/specs/realizar_login.feature
@@ -4,10 +4,15 @@ Funcionalidade: Fazer login
 -Eu como usuario do app
 -Desejo fazer login no app
 
-
-Contexto: 
-    Dado eu esteja na home
+Contexto:  
+    Dado que eu esteja na home
+    Quando eu clico no menu
+    Então clico em "Perfil" do menu 
 
 Cenário: Login com Facebook
+    Quando clico no botão Entrar com Facebook
+    Então modal do Facebook deve ser exibida
+
+
 
 
--- Mobfiq padrão/Cobasi/features/step_definitions/realizar_login_steps.rb
+++ Mobfiq padrão/Cobasi/features/step_definitions/realizar_login_steps.rb
@@ -1,3 +1,25 @@
-Dado("eu esteja na home") do
+  Dado("que eu esteja na home") do
     find_element(id: "open_search_btn")
   end
+
+  Quando("eu clico no menu") do
+    find_element(accessibility_id: "Abrir menu de navegação").click
+  end
+
+  Então("clico em {string} do menu") do |botao_menu|
+    menu_lateral = find_element(id: "nav_menu_nav_view")
+    #if menu_lateral.enabled == true
+      if(botao_menu == "Perfil")
+        find_element(id: "nav_menu_header_text_area").click
+      end
+    #end
+  end
+
+  Quando("clico no botão Entrar com Facebook") do 
+    find_element(id: "fb_login_button").click
+  end
+
+  Então("modal do Facebook deve ser exibida") do
+    find_element(xpath: "//android.webkit.WebView")
+  end
+
